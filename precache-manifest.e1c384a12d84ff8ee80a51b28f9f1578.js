9925370e9dd05914e2a25f926b94f042cb556893
Vishal Kushwaha <vishal@tdgpartner.info>
2020-05-09 10:27:05 +0530

    Updates

---
+++ precache-manifest.e1c384a12d84ff8ee80a51b28f9f1578.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5fae2e5769b013bc18bc285226c38a",
+    "url": "/wondrtech/index.html"
+  },
+  {
+    "revision": "0bfb92e38e16550105f4",
+    "url": "/wondrtech/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5794c26c664bb6906643",
+    "url": "/wondrtech/static/css/main.efdd8622.chunk.css"
+  },
+  {
+    "revision": "0bfb92e38e16550105f4",
+    "url": "/wondrtech/static/js/2.b7b11137.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wondrtech/static/js/2.b7b11137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5794c26c664bb6906643",
+    "url": "/wondrtech/static/js/main.4ad661ef.chunk.js"
+  },
+  {
+    "revision": "382efcfdc7bceb67ff10",
+    "url": "/wondrtech/static/js/runtime-main.e4475a8a.js"
+  },
+  {
+    "revision": "8ad533961f50d1e33b2595a126a3d020",
+    "url": "/wondrtech/static/media/Loader.8ad53396.gif"
+  }
+]);
